9f8003e7b4ca9cade48a3c948c199fe18ce099dc
rokiyama <57606749+rokiyama@users.noreply.github.com>
2020-05-05 18:37:57 +0900

    add mapbox-gl-language

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import mapboxgl from 'mapbox-gl'
+import MapboxLanguage from '@mapbox/mapbox-gl-language'
 import './App.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
@@ -9,8 +10,12 @@ function App() {
     const map = new mapboxgl.Map({
       accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [140, 36],
+      zoom: 8,
     })
+
+    map.addControl(new MapboxLanguage())
 
     return () => {
       console.log('removing mapbox')
