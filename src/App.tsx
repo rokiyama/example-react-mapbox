b5552ea47ee1aaba8034eaa243091e35f6315de8
rokiyama <57606749+rokiyama@users.noreply.github.com>
2020-05-05 15:47:21 +0900

    add mapbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useState } from 'react'
 import './App.css'
+import ReactMapGL from 'react-map-gl'
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 35,
+    longitude: 139,
+    zoom: 8,
+  })
+
+  const mapStyle = 'mapbox://styles/mapbox/streets-v11'
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={setViewport}
+      width='100vw'
+      height='100vh'
+      mapStyle={mapStyle}
+    />
   )
 }
 
